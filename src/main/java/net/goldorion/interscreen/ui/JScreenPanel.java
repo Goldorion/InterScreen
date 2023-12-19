c968950cfe73e2bb5d11cd40cf69bdfce0ae2f89
Goldorion <38427877+Goldorion@users.noreply.github.com>
2023-12-19 11:54:11 -0500

    Resize displayed image
    Fix a problem where bigger screens were not fully displayed.

--- src/main/java/net/goldorion/interscreen/ui/JScreenPanel.java
+++ src/main/java/net/goldorion/interscreen/ui/JScreenPanel.java
@@ -15,13 +15,14 @@ public class JScreenPanel extends JPanel {
 
     public void setImage(BufferedImage image) {
         this.image = image;
+
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (image != null)
-            g.drawImage(image, 0, 0, null);
+            g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
 
         repaint();
     }
